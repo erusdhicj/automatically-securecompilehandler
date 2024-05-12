3fbfb5812fd528c3c02ec07fdefacda45b87b4fe
erusdhicj <amandakimpsg@hotmail.com>
2024-05-13 06:14:38 +0700

    init project

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const countingSort = (arr) => {
-  const min = Math.min(...arr);
-  const max = Math.max(...arr);
-  const count = Array(max - min + 1).fill(0);
-  for (let num of arr) {
-    count[num - min]++;
-  }
-  let sortedIndex = 0;
-  for (let i = min; i <= max; i++) {
-    while (count[i - min] > 0) {
-      arr[sortedIndex++] = i;
-      count[i - min]--;
+function lengthOfLongestSubstring(s) {
+  const map = new Map();
+  let maxLength = 0;
+  let left = 0;
+  for (let right = 0; right < s.length; right++) {
+    if (map.has(s[right])) {
+      left = Math.max(left, map.get(s[right]) + 1);
     }
+    map.set(s[right], right);
+    maxLength = Math.max(maxLength, right - left + 1);
   }
-  return arr;
-};
+  return maxLength;
+}

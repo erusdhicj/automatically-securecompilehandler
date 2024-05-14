3e59ae46f238c2042dc5847f93450f6a91ec3953
erusdhicj <amandakimpsg@hotmail.com>
2024-05-14 09:35:16 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const firstName = "John";
-const lastName = "Doe";
-const fullName = firstName + " " + lastName;
-console.log(fullName); // John Doe
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
